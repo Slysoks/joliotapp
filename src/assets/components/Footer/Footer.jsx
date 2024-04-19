4dbff78e3a54c18e701e02899b9b616d3d64274d
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-04-19 14:48:17 +0200

    - Added highlight on selected category

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,40 +1,46 @@
 import './Footer.scss'
 import { Link } from 'react-router-dom';
 
-function Footer() {
+export default function Footer({ page }) {
     return (
-        <footer>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={'map'}>
-                            <img src="/src/assets/img/icons/map.svg" alt="Carte"/>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={'news'}>
-                            <img src="/src/assets/img/icons/news.svg" alt="News"/>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={'/'}>
-                            <img src="/src/assets/img/icons/home.svg" alt="Accueil"/>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={'menu'}>
-                            <img src="/src/assets/img/icons/menu.svg" alt="Menu"/>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={'account'}>
-                            <img src="/src/assets/img/icons/account.svg" alt="Compte"/>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </footer>
+        <>
+            <footer>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={'map'}>
+                                <img src="/src/assets/img/icons/map.svg" alt="Carte"
+                                     className={page === 'map' ? 'active' : ''}/>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={'news'}>
+                                <img src="/src/assets/img/icons/news.svg" alt="News"
+                                     className={page === 'news' ? 'active' : ''}/>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={'/'}>
+                                <img src="/src/assets/img/icons/home.svg" alt="Accueil"
+                                     className={page === 'home' || page === '' ? 'active' : ''}/>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={'menu'}>
+                                <img src="/src/assets/img/icons/menu.svg" alt="Menu"
+                                     className={page === 'menu' ? 'active' : ''}/>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={'account'}>
+                                <img src="/src/assets/img/icons/account.svg" alt="Compte"
+                                     className={page === 'account' ? 'active' : ''}/>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </footer>
+        </>
+
     )
 }
-
-export default Footer
