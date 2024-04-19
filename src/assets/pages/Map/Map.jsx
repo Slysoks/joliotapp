8dca5f57175a60fcfd057590dc96b5dfe6ccfb5f
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-04-19 19:31:27 +0200

    - Changed the map.jsx code to remove the useless Component.jsx import

--- src/assets/pages/Map/Map.jsx
+++ src/assets/pages/Map/Map.jsx
@@ -1,9 +1,53 @@
-import Component from './Component/Component'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import file from './markers.json'
+import L from 'leaflet';
 
-function Map() {
+
+export default function Map() {
+    const position = [48.12599,-1.64820]; // Set your own coordinates
+
     return (
-        <Component />
+        <MapContainer center={position} zoom={18} style={{height : '100vh'}}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Markers />
+        </MapContainer>
     );
 }
 
-export default Map;
+function getIcon(iconName) {
+    return new L.Icon({
+        iconUrl: `/src/assets/img/icons/map/${iconName}.svg`,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowSize: [41, 41]
+    });
+
+}
+
+function Markers () {
+
+    let markers = [];
+    let i = 0;
+
+    for (const bat in file) {
+        let icon = getIcon(file[bat].icon);
+        markers.push(
+            <Marker position={[file[bat].coordinates.latitude, file[bat].coordinates.longitude]} key={i} icon={icon}>
+                <Popup>
+                    <h1>{file[bat].name}</h1>
+                    <br/>
+                    <h2>{file[bat].description}</h2>
+                </Popup>
+            </Marker>
+        )
+        i++;
+    }
+
+    return markers;
+}
