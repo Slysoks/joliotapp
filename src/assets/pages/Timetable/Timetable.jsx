183d2393b37ef6501fac414844f94015e8b8d2bc
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-05-14 19:59:29 +0200

    - Added timetable example

--- src/assets/pages/Timetable/Timetable.jsx
+++ src/assets/pages/Timetable/Timetable.jsx
@@ -1,10 +1,37 @@
+import './Timetable.scss'
+
 export default function Timetable() {
     const currentDate = new Date();
-    const weekDay = currentDate.toLocaleString('fr-FR', { weekday: 'long' });
+    const weekDay = currentDate.toLocaleString('fr-FR', { weekday: 'short', day: 'numeric', month: 'short' });
   
     return (
         <div>
-            <h1 style={{textTransform: "capitalize"}}>{weekDay}</h1>
+            <nav>
+                <a className={'arrow'} href={'#'}>&#8249;</a>
+                <h1 style={{textTransform: "capitalize", textAlign: "center", fontSize: "26px"}}>{weekDay}</h1>
+                <a className={'arrow'} href={'#'}>&#8250;</a>
+            </nav>
+            <div id={'table'}>
+                <div className={'lesson'}>
+                    <header>
+                        <p className={'lesson-name'}>Math&eacute;matiques</p>
+
+                        <div className={'lesson-time'}>
+                            <p className={'beginning-hour'}>08:10</p>
+                            <p className={'end-hour'}>09:05</p>
+                        </div>
+                    </header>
+
+                    <div className={'lesson-infos'}>
+                        <p className={'room'}>D105</p>
+                        <p className={'teacher'}>M. Dupont</p>
+                        <p className={'time'}>55 min</p>
+                        <div className={'status cancelled'}>
+                            <p className={'status-text'}>Changement de salle</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
